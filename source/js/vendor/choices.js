24cfbefaef026fa754bb9cb998a0a8432067602b
Irina Lukina <ibenderina@gmail.com>
2021-05-07 19:10:02 +0300

    Редактирует страницу События

---
+++ source/js/vendor/choices.js
@@ -0,0 +1,29 @@
+import Choices from 'choices.js';
+
+
+const choiceHandler = (choiceElement) => {
+  choiceElement.passedElement.element.addEventListener(
+    'choice',
+    (evt) => {
+      const selectedValues = choiceElement.getValue();
+      const selectedValue = selectedValues.find((selectedValue) => selectedValue.choiceId === evt.detail.choice.id)
+      if (selectedValue) {
+        setTimeout(() => choiceElement.removeActiveItemsByValue(selectedValue.value), 10)
+      }
+    },
+    false,
+  );
+}
+
+document.querySelectorAll('.input-dropdown')
+  .forEach((item) => {
+    const choiceElement = new Choices(item, {
+      searchEnabled: false,
+      itemSelectText: '',
+      removeItems: false,
+      renderSelectedChoices: 'always',
+    });
+    choiceHandler(choiceElement);
+  });
+
+
