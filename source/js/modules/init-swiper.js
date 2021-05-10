1c5d38c46295af014265f36d5d831c2509c91c17
Valentina <hero.twilight@mail.ru>
2021-05-10 18:58:35 +0300

    добавляет стр. регистрации

--- source/js/modules/init-swiper.js
+++ source/js/modules/init-swiper.js
@@ -14,6 +14,20 @@ const initSwiper = () => {
       prevEl: '.swiper-button-prev',
     },
   });
+
+  //  let swiper1 = new Swiper(".mySwiper1", {
+  //  slidesPerView: 1,
+  //  spaceBetween: 16.5,
+  //  loop: true,
+  //  pagination: {
+  //    el: ".swiper-pagination1",
+  //    clickable: true,
+  //  },
+  //  navigation: {
+  //    nextEl: ".swiper-button-next1",
+  //    prevEl: ".swiper-button-prev1",
+  //  },
+  //});
 };
 
 export {initSwiper};
