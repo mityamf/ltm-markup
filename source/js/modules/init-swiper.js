a9c07ba31b451c9bbca1c1bb9f65a0fe13799688
Irina Lukina <ibenderina@gmail.com>
2021-05-17 11:03:11 +0300

    Рефакторинг

--- source/js/modules/init-swiper.js
+++ source/js/modules/init-swiper.js
@@ -37,10 +37,10 @@ const initSwiper = () => {
       prevEl: '.swiper-button-prev',
     },
     breakpoints: {
-      768: {
+      0: {
         slidesPerView: 1,
       },
-      1280: {
+      768: {
         slidesPerView: 4,
       },
     },
@@ -51,6 +51,7 @@ const initSwiper = () => {
         }, 1);
       },
       resize: function () {
+        console.log(this)
         if (this.isBeginning && this.isEnd) {
           this.navigation.prevEl.setAttribute("hidden", "true");
           this.navigation.nextEl.setAttribute("hidden", "true");
@@ -61,6 +62,60 @@ const initSwiper = () => {
           this.pagination.init();
         }
       }
+    }
+  });
+
+  new Swiper('.event-swiper', {
+    spaceBetween: 0,
+    slidesPerGroup: 1,
+    loop: false,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+    on: {
+      init: function () {
+        setTimeout(() => {
+          window.dispatchEvent(new Event('resize'));
+        }, 1);
+      },
+      resize: function () {
+        console.log(this)
+        if (this.isBeginning && this.isEnd) {
+          this.navigation.prevEl.setAttribute("hidden", "true");
+          this.navigation.nextEl.setAttribute("hidden", "true");
+          this.pagination.destroy();
+        } else {
+          this.navigation.prevEl.removeAttribute("hidden");
+          this.navigation.nextEl.removeAttribute("hidden");
+          this.pagination.init();
+        }
+      }
+    }
+  });
+
+  new Swiper('.photos-swiper', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     }
   });
 
