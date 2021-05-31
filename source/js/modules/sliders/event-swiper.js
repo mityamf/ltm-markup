0f3fa4a764444c47d9a803a77679f68f7cc087f1
Irina Lukina <ibenderina@gmail.com>
2021-05-31 09:05:30 +0300

    Рефакторинг, правки

---
+++ source/js/modules/sliders/event-swiper.js
@@ -0,0 +1,47 @@
+const eventSwiper = () => {
+  return new Swiper('.direction__slider--event', {
+    spaceBetween: 0,
+    slidesPerGroup: 1,
+    loop: false,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.direction__pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.direction__arrow--right',
+      prevEl: '.direction__arrow--left',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+    on: {
+      init() {
+        setTimeout(() => {
+          window.dispatchEvent(new Event('resize'));
+        }, 1);
+      },
+      resize() {
+        if (this.isBeginning && this.isEnd) {
+          this.navigation.prevEl.setAttribute('hidden', 'true');
+          this.navigation.nextEl.setAttribute('hidden', 'true');
+          this.pagination.destroy();
+        } else {
+          this.navigation.prevEl.removeAttribute('hidden');
+          this.navigation.nextEl.removeAttribute('hidden');
+          this.pagination.init();
+        }
+      },
+    },
+  });
+};
+
+export {eventSwiper};
